1d11214c7d8e5e312a85732bcf19c13b4ff12d44
EvgeniOvcharov <30571070+EvgeniOvcharov@users.noreply.github.com>
2021-03-24 17:09:17 +0200

    Adding pdf materials

--- iter-methods.R
+++ iter-methods.R
@@ -1,12 +1,13 @@
-# This function implements the Jackobi iteration
+# This function implements the Jacobi iteration
+# for finding the solution to the equation Ax = b
 
-Jackobi = function(A, b, tol = 0.0001, K = 100) {
+Jacobi = function(A, b, x0 = 0, tol = 0.0001, K = 100) {
   d = diag(A)
   T = A
   diag(T) = 0
   
   n = nrow(T)
-  x = numeric(n)
+  x = numeric(n) + x0
   k = 0; err = 1
   
   while(k < K & err > tol) {
@@ -18,4 +19,14 @@ Jackobi = function(A, b, tol = 0.0001, K = 100) {
   }
   print(k)
   return(x)
+}
+
+# The convergence of the Jacobi iteration depends on the principle eigenvalue of the matrix T,
+# T = D^{-1}(L + U), where A is decomposed as A = D - L - U
+ 
+max_eigen_jacobi = function(A){
+  D = diag(diag(A))
+  T = D - A
+  T = solve(D) %*% T
+  abs(eigen(T)$values[1])
 }
